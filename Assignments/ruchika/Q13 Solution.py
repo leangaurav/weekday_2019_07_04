497993052496ec0e0e0505fef95bd20de9253e3d
Gusain <ruchika.gusain@siemens.com>
2019-07-08 15:01:33 +0530

    Ruchika First Assignment

---
+++ Assignments/ruchika/Q13 Solution.py
@@ -0,0 +1,3 @@
+import math
+num= int(input('Enter a number'))
+math.sqrt(num)
---
+++ Assignments/ruchika/Q8 Solution.py
@@ -0,0 +1,5 @@
+strOrig=input('Enter the string:')
+n=int(input('Enter the number:'))
+print('String ',strOrig, ' is printing  ',n, ' times on the screen')
+newStr= strOrig +'\n'
+print(newStr * n)
