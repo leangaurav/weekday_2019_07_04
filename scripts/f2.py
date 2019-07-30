85bdff82440986133f6357c02d9750546ec8dea3
guptag <trainer.cpp@gmail.com>
2019-07-30 22:53:49 +0530

    Session 17 Notebook, PPT, Assignments

--- scripts/f2.py
+++ scripts/f2.py
@@ -1,1 +1,21 @@
-print("Hello")
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.figure()
+a = np.random.randint(1,100,100)
+b = np.random.randint(1,100,100)
+c = np.random.randint(1,100,100)
+plt.scatter(a,b,s = 5, alpha = 0.7)
+plt.scatter(a,c,s = 5, alpha = 0.7)
+
+plt.show()
+
+plt.figure()
+a = np.random.randint(1,100,100)
+b = np.random.randint(1,100,100)
+c = np.random.randint(1,100,100)
+plt.scatter(a,b,s = 5, alpha = 0.7)
+plt.scatter(a,c,s = 5, alpha = 0.7)
+
+plt.show()
+
